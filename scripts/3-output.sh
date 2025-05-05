e98d8c7f2acd62d7e9253f363b14d9e5d2092f9d
phillipakhn <phillipakhn@gmail.com>
2025-05-05 00:54:26 -0700

    Output

--- scripts/3-output.sh
+++ scripts/3-output.sh
@@ -11,3 +11,5 @@ echo "[Output] Saving HDFS output to $LOCAL_OUTPUT_FILE..."
 hdfs dfs -cat /output-local/part-r-00000 > "$LOCAL_OUTPUT_FILE"
 
 echo "[Output] Done. Output saved to: $LOCAL_OUTPUT_FILE"
+
+hdfs dfs -cat /output-local/part-r-00000
