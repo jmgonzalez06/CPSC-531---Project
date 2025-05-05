933b93dda4357568edf46c6b781270c84bf35529
phillipakhn <phillipakhn@gmail.com>
2025-05-05 02:43:14 -0700

    Updated output

--- scripts/3-output.sh
+++ scripts/3-output.sh
@@ -2,13 +2,18 @@
 set -e
 
 LOCAL_OUTPUT_DIR="./output"
-LOCAL_OUTPUT_FILE="$LOCAL_OUTPUT_DIR/movie_results.csv"
+#LOCAL_OUTPUT_FILE="$LOCAL_OUTPUT_DIR/movie_results.csv"
+#LOCAL_OUTPUT_FILE="$LOCAL_OUTPUT_DIR/movie_results.txt"
 
 echo "[Output] Creating local output directory: $LOCAL_OUTPUT_DIR"
 mkdir -p "$LOCAL_OUTPUT_DIR"
 
 echo "[Output] Saving HDFS output to $LOCAL_OUTPUT_FILE..."
-hdfs dfs -cat /output-local/part-r-00000 > "$LOCAL_OUTPUT_FILE"
+#hdfs dfs -cat /output-local/part-r-00000 > "$LOCAL_OUTPUT_FILE"
+
+hdfs dfs -cat /output-local/part-r-00000 > "$LOCAL_OUTPUT_DIR/movie_results.csv"
+hdfs dfs -cat /output-local/part-r-00000 > "$LOCAL_OUTPUT_DIR/movie_results.txt"
+
 
 echo "[Output] Done. Output saved to: $LOCAL_OUTPUT_FILE"
 
